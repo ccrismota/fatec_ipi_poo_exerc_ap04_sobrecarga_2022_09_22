402dc6b246e32e20926cd0a9ec2d3fe68ef9d18f
ccrismota <ccrismota@gmail.com>
2022-09-22 18:22:53 -0300

    Resolucao exercicio apostila04: Criacao da classe Imprimir e metodos exibir

---
+++ Impressora.java
@@ -0,0 +1,32 @@
+public class Impressora {
+
+    public void exibir(float a){
+        System.out.println(a);
+    }
+    
+    
+    public void exibir(float a, float b){
+        System.out.println(a + "," + b);
+    }
+
+
+    public void exibir(float a, String s){
+        System.out.println(a + "," + s);
+    }
+
+    public void exibir(String s, float a){
+        System.out.println(a + "," + s);
+    }
+        
+    public void exibir(String s1,String s2, String s3){
+        System.out.println( s2 + "," + s3 + "," + s3 );
+    }
+
+    public void exibir(int a, int b, String s){
+        System.out.println(a + "," + b + "," + s);
+    }
+
+    
+
+     
+}
